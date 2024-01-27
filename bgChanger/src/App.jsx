9432d314d1c30b163f9248a337539cc5cce22321
RajeshVerma1612 <verma.rajesh123v@gmail.com>
2024-01-27 11:00:59 +0530

    Background Changer Project

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -5,16 +5,18 @@ import './App.css'
 import BgChanger from './components/BgChanger'
 
 function App() {
-  let [bg, setBg] = useState("Black")
-  const bgColours = ["red", "green", "blue", "purple", "yellow", "orange", "pink", "gray"]
-  let rand = Math.floor(Math.random() * 8 + 1)
+  let [colour, setColour] = useState("Black")
+  const bgColours = ["red", "green", "blue", "purple", "yellow", "orange", "pink", "gray","lavender"]
   function nextBg() {
-      bg=bgColours[rand]
-      setBg(bg)
+    let rand = Math.floor(Math.random() * 10)
+    colour=bgColours[rand]
+    if(colour===undefined)
+      colour="white"
+    setColour(colour)
   }
   return (
-    <div className=>
-      <button onClick={nextBg} className='p-4 bg-white text-black border-8 border-black rounded font-bold text-xl'>Hello World</button>
+    <div className='w-full h-screen flex justify-center items-center' style={{backgroundColor:colour}}>
+      <button onClick={nextBg} className='p-4 bg-white text-black border-8 border-black rounded font-bold text-xl'>{colour}</button>
       {/* <BgChanger/> */}
     </div>
   )
